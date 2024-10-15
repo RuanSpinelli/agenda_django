5e6e62c0cd25d426eafd68a5a194620d8c11bd8a
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2024-10-15 17:32:57 -0300

    commit 3
    mais algumas modificações foram feitas e mais informações foram inseridas no banco de dados

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,9 +7,10 @@ from django.db import models
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField()
-    data_criacao = models.DateTimeField(auto_now=True)
-
+    data_evento = models.DateTimeField(verbose_name="Data do evento")
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name="Data que você anotou")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
     #forçar o nome da tabela no db ser o mesmo nome da classe
     class Meta:
